a6a545f779a74a36d88dd32be841c93202625734
Jiting <jiting@jtcat.com>
2024-12-02 16:29:22 +0800

    update currect upstream changes expect drop 3.0
    ref: https://github.com/docker-library/ruby/commit/cfb3148e13d2952e0a88863719959a6ec0ff0098

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-declare -A aliases=(
-	[3.2]='3 latest'
-)
-
 self="$(basename "$BASH_SOURCE")"
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
@@ -44,17 +40,19 @@ dirCommit() {
 
 getArches() {
 	local repo="$1"; shift
-	local officialImagesUrl='https://github.com/docker-library/official-images/raw/master/library/'
+	local officialImagesBase="${BASHBREW_LIBRARY:-https://github.com/docker-library/official-images/raw/HEAD/library}/"
 
-	eval "declare -g -A parentRepoToArches=( $(
-		find -name 'Dockerfile' -exec awk '
+	local parentRepoToArchesStr
+	parentRepoToArchesStr="$(
+		find -name 'Dockerfile' -exec awk -v officialImagesBase="$officialImagesBase" '
 				toupper($1) == "FROM" && $2 !~ /^('"$repo"'|scratch|.*\/.*)(:|$)/ {
-					print "'"$officialImagesUrl"'" $2
+					printf "%s%s\n", officialImagesBase, $2
 				}
 			' '{}' + \
 			| sort -u \
-			| xargs bashbrew cat --format '[{{ .RepoName }}:{{ .TagName }}]="{{ join " " .TagEntry.Architectures }}"'
-	) )"
+			| xargs -r bashbrew cat --format '["{{ .RepoName }}:{{ .TagName }}"]="{{ join " " .TagEntry.Architectures }}"'
+	)"
+	eval "declare -g -A parentRepoToArches=( $parentRepoToArchesStr )"
 }
 getArches 'ruby'
 
@@ -71,18 +69,34 @@ join() {
 	echo "${out#$sep}"
 }
 
+declare -A latest=(
+	#[3]='3.2'
+	#[latest]='3.2'
+)
+
 for version; do
 	export version
+
+	if ! fullVersion="$(jq -er '.[env.version] | if . then .version else empty end' versions.json)"; then
+		continue
+	fi
+
 	variants="$(jq -r '.[env.version].variants | map(@sh) | join(" ")' versions.json)"
 	eval "variants=( $variants )"
-
-	fullVersion="$(jq -r '.[env.version].version' versions.json)"
 
 	versionAliases=(
 		$fullVersion
 		$version
-		${aliases[$version]:-}
 	)
+
+	if [ "$version" = "${version%-rc}" ]; then
+		for a in "${version%%.*}" latest; do # "3", "latest"
+			if [ -z "${latest[$a]:-}" ]; then
+				latest[$a]="$version"
+				versionAliases+=( "$a" )
+			fi
+		done
+	fi
 
 	defaultDebianVariant="$(jq -r '
 		.[env.version].variants
